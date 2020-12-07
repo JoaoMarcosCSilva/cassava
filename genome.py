f3c2ab6d47e8da29ed1715c2ce4672c3f49813d6
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-12-07 11:35:01 -0300

    Fixes that again

--- genome.py
+++ genome.py
@@ -48,7 +48,8 @@ class Genome:
             im = tf.image.random_contrast(im, self.contrast_lower, self.contrast_upper)
             im = tf.image.random_hue(im, self.hue_max_delta)
             im = tf.image.random_saturation(im, self.saturation_lower, self.saturation_upper)
-        return augment, lbl
+            return im, lbl
+        return augment
 
     def evaluate(self):
         if self.score == None:
