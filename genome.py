b166b237e9a000caeee09628e00f53f017eca5fd
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-12-07 11:25:38 -0300

    Initial commit

---
+++ genome.py
@@ -0,0 +1,80 @@
+import numpy as np
+import json
+import tensorflow as tf
+
+class Genome:
+    def __init__(self):
+        self.score = None
+        self.mutate(1)
+
+    def mutate(self, probability):
+        def p():
+            return np.random.uniform(0,1) < probability
+        
+        if p():
+            self.crop_size = np.random.randint(300, 512)
+        if p():
+            self.brightness_max_delta = np.random.uniform(0, 0.5)
+        if p():
+            self.contrast_lower = 0
+            self.contrast_upper = np.random.uniform(self.contrast_lower, 1)
+        if p():
+            self.hue_max_delta = np.random.uniform(0, 0.5)
+        if p():
+            self.saturation_lower = 0
+            self.saturation_upper = np.random.uniform(self.saturation_lower, 1)
+        if p():
+            self.log_lr_1 = np.random.uniform(-2, 0)
+            self.lr1 = 10**self.log_lr_1
+            self.log_lr_2 = np.random.uniform(-4, self.log_lr_1)
+            self.lr2 = 10**self.log_lr_2
+        if p():
+            self.neg_log_momentum = np.random.uniform(-5, -1)
+            self.momentum = 1 - 10**self.neg_log_momentum
+        if p():
+            self.dropout = np.random.uniform(0, 0.5)
+        if p():
+            self.log_l2 = np.random.uniform(-5, -1)
+            self.l2 = 10**self.log_l2
+
+    def get_augment(self):
+        def augment(im, lbl):
+            im = tf.image.random_flip_left_right(im)
+            im = tf.image.random_flip_up_down(im)
+            
+            im = tf.image.resize(tf.image.random_crop(im, (self.crop_size,self.crop_size,3)), (512,512))
+            
+            im = tf.image.random_brightness(im, self.brightness_max_delta)
+            im = tf.image.random_contrast(im, self.contrast_lower, self.contrast_upper)
+            im = tf.image.random_hue(im, self.hue_max_delta)
+            im = tf.image.random_saturation(im, self.saturation_lower, self.saturation_upper)
+        return augment
+
+    def evaluate(self):
+        if self.score == None:
+            self.score = train.train_model(self)
+        return self.score
+
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.__dict__, f, indent = 2)
+
+    def read(self, filename):
+        with open(filename, 'r') as f:
+            d = json.load(f)
+            self.__dict__ = d
+
+def crossover(a, b):
+    c = Genome()
+    
+    halves = ['brightness_max_delta', 'contrast_lower', 'contrast_upper', 'hue_max_delta', 'saturation_lower', 'saturation_upper', 'log_lr_1', 'log_lr_2', 'neg_log_momentum', 'dropout', 'log_l2']
+    c.crop_size = (a.crop_size + b.crop_size) // 2
+    for key in halves:
+        c.__dict__[key] = a.__dict__[key] + b.__dict__[key]
+
+    c.lr1 = 10**c.log_lr_1
+    c.lr2 = 10**c.log_lr_2
+    
+    c.momentum = 1 - 10**c.neg_log_momentum
+    c.l2 = 10**c.log_l2
+    return c
---
+++ population.py
@@ -0,0 +1,44 @@
+from genome import *
+import json
+import copy
+
+class Population:
+    def __init__(self, size):
+        self.individuals = [[Genome() for i in range(size)]]
+        self.evaluations = []
+
+    def generation(self, mutation_rate):
+        evaluation = [g.evaluate() for g in self.individuals[-1]]
+        self.evaluations.append(evaluation)
+        
+        best_individual = np.argmax(evaluation)
+
+        new_individuals = copy.deepcopy(self.individuals[-1])
+
+        for i in range(len(new_individuals)):
+            if i == best_individual:
+                continue
+            else:
+                new_individuals[i] = crossover(new_individuals[i], new_individuals[best_individual])
+                new_individuals[i].mutate(mutation_rate)
+        self.individuals.append(new_individuals)
+
+    def save(self, filename):
+        d = copy.deepcopy(self.__dict__)
+        for i in range(len(d['individuals'])):
+            for j in range(len(d['individuals'][i])):
+                d['individuals'][i][j] = d['individuals'][i][j].__dict__
+
+        with open(filename, 'w') as f:
+            json.dump(d, f, indent = 2)
+
+    def load(self, filename):
+        with open(filename, 'r') as f:
+            self.__dict__ = json.load(f)
+        for i in range(len(self.individuals)):
+            for j in range(len(self.individuals[i])):
+                k = Genome()
+                k.__dict__ = self.individuals[i][j]
+                self.individuals[i][j] = k
+                
+
