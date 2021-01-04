03715b8a479d35c900357450d5f9f5a1b0393735
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2021-01-04 10:38:26 -0300

    Adds image augmentations and removes L2 regularization

--- genome.py
+++ genome.py
@@ -10,7 +10,7 @@ class Genome:
     def mutate(self, probability):
         def p():
             return np.random.uniform(0,1) < probability
-        
+    
         if p():
             self.crop_size = np.random.randint(300, 512)
         if p():
@@ -34,10 +34,11 @@ class Genome:
         if p():
             self.dropout = np.random.uniform(0, 0.5)
         if p():
-            self.log_l2 = np.random.uniform(-5, -1)
-            self.l2 = 10**self.log_l2
-        if p():
             self.smoothing = np.random.uniform(0,0.2)
+        if p():
+            self.cutout_size = np.random.randint(0, 256)
+        if p():
+            self.radians = np.random.uniform(0, np.pi/2)
 
     def get_augment(self):
         def augment(im, lbl):
@@ -50,6 +51,10 @@ class Genome:
             im = tf.image.random_contrast(im, self.contrast_lower, self.contrast_upper)
             im = tf.image.random_hue(im, self.hue_max_delta)
             im = tf.image.random_saturation(im, self.saturation_lower, self.saturation_upper)
+            
+            im = tfa.image.random_cutout(tf.expand_dims(im,0), (self.cutout_size,self.cutout_size))[0]
+            im = tfa.image.rotate(im, tf.random.uniform([], -self.radians, self.radians))
+            
             return im, lbl
         return augment
 
@@ -66,12 +71,13 @@ class Genome:
         with open(filename, 'r') as f:
             d = json.load(f)
             self.__dict__ = d
-
+                        
 def crossover(a, b):
     c = Genome()
     
-    halves = ['brightness_max_delta', 'contrast_lower', 'contrast_upper', 'hue_max_delta', 'saturation_lower', 'saturation_upper', 'log_lr_1', 'log_lr_2', 'neg_log_momentum', 'dropout', 'log_l2', 'smoothing']
+    halves = ['brightness_max_delta', 'contrast_lower', 'contrast_upper', 'hue_max_delta', 'saturation_lower', 'saturation_upper', 'log_lr_1', 'log_lr_2', 'neg_log_momentum', 'dropout', 'radians', 'smoothing']
     c.crop_size = (a.crop_size + b.crop_size) // 2
+    c.cutout_size = (a.cutout_size + b.cutout_size) // 2
     for key in halves:
         c.__dict__[key] = (a.__dict__[key] + b.__dict__[key]) / 2
 
@@ -79,5 +85,4 @@ def crossover(a, b):
     c.lr2 = 10**c.log_lr_2
     
     c.momentum = 1 - 10**c.neg_log_momentum
-    c.l2 = 10**c.log_l2
     return c
